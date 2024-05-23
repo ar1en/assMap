3690e4cc0ba9db47e4333d9853ebc49926d0c0c1
ar1en <ar1en@hotmail.com>
2024-05-23 18:04:44 +0300

    миграция репо 0.3

---
+++ db/scripts/ent_processes_types(data).sql
@@ -0,0 +1,1 @@
+INSERT INTO public.ent_processes_types (id, name, author, created_at, updated_at, deleted_at) VALUES ('0f10f1ff-8fee-4c98-978f-7b73ccbf5708', 'Тестовый тип 1', '30b0310d-15bd-41b4-bd1d-17d5a875a6f6', '2024-02-02 10:15:59', '2024-02-02 10:16:03', null);
